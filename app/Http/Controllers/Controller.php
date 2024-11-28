d4452e7d80ec584775778e023da91fba4a3d5c17
mindWhisperer <148146743+mindWhisperer@users.noreply.github.com>
2024-11-28 15:59:15 +0100

    index s databazkou

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,38 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Providers\BookServiceProvider;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\View\View;
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
+
+    private BookServiceProvider $bookService;
+
+    public function __construct()
+    {
+        $this->bookService = new BookServiceProvider();
+    }
+
+    public function index():View
+    {
+        $newest = $this->bookService->lastThree();
+        return view('index', ['books' => $newest]);
+    }
+
+    public function detail($id)
+    {
+        $book = $this->bookService->read($id);
+        return view('detail', ['book' => $book]);
+    }
+
+    public function stalice()
+    {
+        $allBooks = $this->bookService->readAll();
+        return view('stalice', ['books' => $allBooks]);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('BckIndex');
-});
+Route::get('/', [\App\Http\Controllers\Controller::class, 'index']);
+
 Route::get('/stalice', function () {
     return view('BckStalice');
 });
